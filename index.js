39b0ec8e8501cb9a0cbd3a24f12ab477758dd0f4
Katherine Tsai <katherinetsai@DESKTOP-10SDEV1.localdomain>
2022-11-01 22:34:59 -0400

    commit tree 535052fc52a528b97363e2a419ba7c225a3367ce

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const main = document.querySelector('main#main')
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+
+newHeader.textContent = "Katherine is the champion";
